6ddb6356663c41038aba06c56c22f30da291ad80
Shanni <shan.liu03@gmail.com>
2021-07-29 14:59:39 -0700

    add starting script

---
+++ agamemnon-cluster-bkp.sh
@@ -0,0 +1,13 @@
+go run src/server/agamemnon-server.go 3331 servers.txt &
+go run src/server/agamemnon-server.go 3332 servers.txt &
+go run src/server/agamemnon-server.go 3333 servers.txt &
+go run src/server/agamemnon-server.go 3334 servers.txt &
+go run src/server/agamemnon-server.go 3335 servers.txt &
+go run src/server/agamemnon-server.go 3336 servers.txt &
+go run src/server/agamemnon-server.go 3337 servers.txt &
+go run src/server/agamemnon-server.go 3338 servers.txt
+
+#go run src/server/agamemnon-server.go 3331 two-servers.txt &
+#go run src/server/agamemnon-server.go 3332 two-servers.txt
+
+#go run src/server/agamemnon-server.go 3331 single-server.txt
